6cd8de0e61a414b4c156fc43430db5ce67e7c358
InexhaustibleSnake <guchmazoval@gmail.com>
2024-11-08 14:12:14 +0300

    Intial commit

---
+++ build.bat
@@ -0,0 +1,6 @@
+"F:\Develop\UE\Engine\UE_5.4\Engine\Build\BatchFiles\RunUAT.bat" ^
+BuildCookRun ^
+-project=E:\Develop\UE\Projects\JenkinsTest\JenkinsTest.uproject ^
+-platform=Win64 -clientconfig=Development ^
+-archivedirectory=E:\Develop\UE\Projects\JenkinsTest\BuildFromBat ^
+-cook -stage -archive -pak -package -allmaps
